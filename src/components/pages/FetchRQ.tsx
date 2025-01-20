cba98d94e774ae892a8bfd9e53ddfcb57791cf29
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-20 14:28:35 +0530

    Implement post fetching with React Query in FetchRQ component fetching api data using React Query

--- src/components/pages/FetchRQ.tsx
+++ src/components/pages/FetchRQ.tsx
@@ -1,5 +1,52 @@
+import { useEffect } from "react";
+import { fetchPosts } from "../API/api";
+import { useQuery } from "@tanstack/react-query";
+
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
+
 const FetchRQ = () => {
-  return <h1 className="text-3xl font-bold">FetchRQ</h1>;
+  const getPostsData = async () => {
+    try {
+      const res = await fetchPosts();
+
+      console.log(res);
+
+      return res.data;
+    } catch (err) {
+      console.log(err);
+      return [];
+    }
+  };
+
+  useEffect(() => {
+    getPostsData();
+  }, []);
+
+  const { data: posts } = useQuery<Post[]>({
+    queryKey: ["posts"], // useState alternative working
+    queryFn: getPostsData, // useEffect alternative working
+    refetchInterval: 10000, // 10 seconds
+  });
+
+  return (
+    <div>
+      <ul className="section-accordion">
+        {posts?.map((curElem) => {
+          const { id, title, body } = curElem;
+          return (
+            <li key={id}>
+              <p>{title}</p>
+              <p>{body}</p>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default FetchRQ;
