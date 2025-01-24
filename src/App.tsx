78e5fbef2e9b628c46017742e19e860d52004396
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-24 14:37:29 +0530

    Add route for individual post retrieval in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import MainLayout from "./components/MainLayout";
 import Home from "./components/pages/Home";
-import FetchOld from "./components/pages/FetchOld";
+import { FetchOld } from "./components/pages/FetchOld";
 import FetchRQ from "./components/pages/FetchRQ";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { FetchIndv } from "./components/pages/FetchIndv";
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -35,6 +36,10 @@ const router = createBrowserRouter([
       {
         path: "/rq",
         element: <FetchRQ />,
+      },
+      {
+        path: "/rq/:id",
+        element: <FetchIndv />,
       },
     ],
   },
