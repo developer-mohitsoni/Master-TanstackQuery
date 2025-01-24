f7eb11df529452367ad70e753127e2a64e06c8bd
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-24 14:37:17 +0530

    Implement pagination in FetchRQ component and add loading/error handling

--- src/components/pages/FetchRQ.tsx
+++ src/components/pages/FetchRQ.tsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import { fetchPosts } from "../API/api";
 import { useQuery } from "@tanstack/react-query";
+import { NavLink } from "react-router-dom";
 
 interface Post {
   id: number;
@@ -8,15 +10,25 @@ interface Post {
 }
 
 const FetchRQ = () => {
-  const { data: posts } = useQuery<Post[]>({
-    queryKey: ["posts"], // useState alternative working
-    queryFn: fetchPosts, // useEffect alternative working
+  const [pageNumber, setPageNumber] = useState(0);
+  const {
+    data: posts,
+    isPending,
+    isError,
+    error,
+  } = useQuery<Post[]>({
+    queryKey: ["posts", pageNumber], // useState alternative working
+    queryFn: () => fetchPosts(pageNumber), // useEffect alternative working
     // gcTime: 1000, // 10 seconds
     // staleTime: 1000 * 60 * 60, // 1sec * 60  => 1 minute * 60 => 1hr in milliseconds
     // refetchIntervalInBackground: true,
     refetchInterval: 1000, // 1 seconds
     refetchIntervalInBackground: true, // fetching the data even swicthing the tabs
   });
+
+  // Conditional rendering based on loading, error, and posts data
+  if (isPending) return <p>Loading...</p>;
+  if (isError) return <p> Error: {error.message || "Something went wrong!"}</p>;
 
   return (
     <div>
@@ -25,12 +37,25 @@ const FetchRQ = () => {
           const { id, title, body } = curElem;
           return (
             <li key={id}>
-              <p>{title}</p>
-              <p>{body}</p>
+              <NavLink to={`/rq/${id}`}>
+                <p>{id}</p>
+                <p>{title}</p>
+                <p>{body}</p>
+              </NavLink>
             </li>
           );
         })}
       </ul>
+      <div className="pagination-section container">
+        <button
+          disabled={pageNumber === 0 ? true : false}
+          onClick={() => setPageNumber((prev) => prev - 3)}
+        >
+          Prev
+        </button>
+        <p>{pageNumber / 3 + 1}</p>
+        <button onClick={() => setPageNumber((prev) => prev + 3)}>Next</button>
+      </div>
     </div>
   );
 };
